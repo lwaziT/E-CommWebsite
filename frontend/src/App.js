4e77d985f5c1e16b36cec1a2ab53a463042dfd61
lwazi <toboselwazi9@gmail.com>
2023-09-20 16:20:32 +0200

    Completed Login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import "./App.css";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {LoginPage, SignupPage} from "./Routes.js"
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/login' element={<LoginPage/>} />
+      <Route path='/sign-up' element={<SignupPage/>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
