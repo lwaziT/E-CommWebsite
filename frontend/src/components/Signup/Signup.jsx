ed800b386703dcd9877704f1d4cfce81b7695111
lwazi <toboselwazi9@gmail.com>
2023-09-20 16:52:03 +0200

    Completed Signup page

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import styles from "../../styles/styles.js";
 import { Link } from "react-router-dom";
-import {RxAvatar} from "react-icons/rx";
+import { RxAvatar } from "react-icons/rx";
 
 const Signup = () => {
   const [name, setName] = useState("");
@@ -32,7 +32,7 @@ const Signup = () => {
           <form className="space-y-6">
             <div>
               <label
-                htmlFor="email"
+                htmlFor="name"
                 className="block text-sm font-medium text-gray-700"
               >
                 Full Name
@@ -103,7 +103,6 @@ const Signup = () => {
                 )}
               </div>
             </div>
-
             <div>
               <label
                 htmlFor="avatar"
@@ -111,17 +110,31 @@ const Signup = () => {
               ></label>
               <div className="mt-2 flex items-center">
                 <span className="inline-block h-8 w-8 rounded-full overflow-hidden">
-                    {
-                        avatar ? (
-                            <img src={URL.createObjectURL(avatar)} className="h-full w-full object-cover rounded-full" />
-                        ) : (
-                            <RxAvatar className="h-8 w-8" />
-                        )
-                    }
+                  {avatar ? (
+                    <img
+                      src={URL.createObjectURL(avatar)}
+                      className="h-full w-full object-cover rounded-full"
+                    />
+                  ) : (
+                    <RxAvatar className="h-8 w-8" />
+                  )}
                 </span>
+                <label
+                  htmlFor="file-input"
+                  className="ml-5 flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+                >
+                  <span>Upload a file</span>
+                  <input
+                    type="file"
+                    name="avatar"
+                    id="file-input"
+                    accept=".jpg,.jpeg,.png"
+                    onChange={handleFileInputChange}
+                    className="sr-only"
+                  />
+                </label>
               </div>
             </div>
-
             <div>
               <button
                 type="submit"
@@ -131,9 +144,9 @@ const Signup = () => {
               </button>
             </div>
             <div className={`${styles.noramlFlex} w-full`}>
-              <h4>Don't have an account yet? </h4>
-              <Link to="/sign-up" className="text-blue-600" pl-2>
-                Sign Up
+              <h4>Already have an account yet? </h4>
+              <Link to="/login" className="text-blue-600" pl-2>
+                Sign in
               </Link>
             </div>
           </form>
