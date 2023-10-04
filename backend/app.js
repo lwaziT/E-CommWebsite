4a1b1112834758cc9ca0e2058732dfd644fbae62
lwazi <toboselwazi9@gmail.com>
2023-10-04 21:03:51 +0200

    Setup backend

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const Errorhandler = require("./utils/ErrorHandler");
 const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+
+app.use(express.json());
+app.use(cookieParser);
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(fileUpload({useTempFiles: true}));
 
 if(process.env.NODE_ENV !== "PRODUCTION"){
     require("dotenv").config({
         path:"backend/config/.env"
     })
 }
+// For ErrorHandling
+app.use(Errorhandler)
 
 module.exports = app;
