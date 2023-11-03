113e0a7b1a74766a97481fa32650510cfd3c723c
lwazi <toboselwazi9@gmail.com>
2023-11-03 12:20:15 +0200

    added rout to controller uninstalled npm fileupload added multer

--- backend/app.js
+++ backend/app.js
@@ -3,18 +3,25 @@ const Errorhandler = require("./utils/ErrorHandler");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const fileUpload = require("express-fileupload");
+
 
 app.use(express.json());
 app.use(cookieParser);
+app.use("/", express.static("uploads"));
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(fileUpload({useTempFiles: true}));
+
 
 if(process.env.NODE_ENV !== "PRODUCTION"){
     require("dotenv").config({
         path:"backend/config/.env"
     })
 }
+
+// Import routes
+const user = require("./controller/user");
+
+app.use("/api/v2/user", user);
+
 // For ErrorHandling
 app.use(Errorhandler)
 
--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,3 +1,27 @@
 const express = require("express");
 const path = require("path");
+const { upload } = require("../multer");
+const Errorhandler = require("../utils/ErrorHandler");
 const router = express.Router();
+
+router.post("create-user", upload.single("file"), async (req,res,next) => {
+    const {name,email,password} = req.body;
+    const userEmail = await User.findOne({email});
+
+    if (userEmail){
+        return next(new Errorhandler("User already exists", 400));
+    }
+
+    const filename = req.file.filename;
+    const fileUrl  = path.join(filename);
+    const user = {
+        name: name,
+        email: email,
+        password: password,
+        avatar: fileUrl,
+    };
+
+    console.log(user);
+})
+
+module.exports = router;
