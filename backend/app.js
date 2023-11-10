53093115af8bde36f3b8f4434330325ba627ffd0
lwazi <toboselwazi9@gmail.com>
2023-11-10 10:56:29 +0200

    Added clientside folder Deleted frontend folder

--- backend/app.js
+++ backend/app.js
@@ -3,19 +3,18 @@ const Errorhandler = require("./utils/ErrorHandler");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+//const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieParser);
-app.use(cors());
+//app.use(cors());
 app.use("/", express.static("uploads"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-if(process.env.NODE_ENV !== "PRODUCTION"){
-    require("dotenv").config({
-        path:"backend/config/.env"
-    })
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "backend/config/.env",
+  });
 }
 
 // Import routes
@@ -24,6 +23,6 @@ const user = require("./controller/user");
 app.use("/api/v2/user", user);
 
 // For ErrorHandling
-app.use(Errorhandler)
+app.use(Errorhandler);
 
 module.exports = app;
